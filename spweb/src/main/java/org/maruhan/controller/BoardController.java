e8232b4f085a0c8d749dfedbaecb5ef411cd8d6e
maruhan <didhddldlq@naver.com>
2015-12-03 15:47:28 +0900

    Create Board Modify by Maruhan 3:47

--- spweb/src/main/java/org/maruhan/controller/BoardController.java
+++ spweb/src/main/java/org/maruhan/controller/BoardController.java
@@ -2,10 +2,12 @@ package org.maruhan.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.maruhan.domain.BoardVO;
 import org.maruhan.service.BoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
